449afa6c759c4ec26a218a5be1c4677427026be2
Rio Nyx <58178550+Rio-Nyx@users.noreply.github.com>
2023-03-08 12:22:56 +0530

    Update sign_network.py

--- sign_network.py
+++ sign_network.py
@@ -6,9 +6,7 @@ class Signmodel(nn.Module):
     def __init__(self, no_classes):
         super(Signmodel, self).__init__()
         self.loss['CTCLoss'] = torch.nn.CTCLoss(reduction='none', zero_infinity=False)
-        self.conv2d = models.resnet18(pretrained=True)loss += weight * self.loss['CTCLoss'](ret_dict["conv_logits"].log_softmax(-1),
-                                                      label.cpu().int(), ret_dict["feat_len"].cpu().int(),
-                                                      label_lgt.cpu().int()).mean(
+        self.conv2d = models.resnet18(pretrained=True)
         out_of_resnet = self.conv2d.fc.in_features
         self.conv2d.fc = nn.Linear(in_features=out_of_resnet, out_features=no_classes)
 
@@ -22,7 +20,6 @@ class Signmodel(nn.Module):
 
     def criterion_calculation(self, ret_dict, label, label_lgt):
         # loss of lstm only need to be considered at end
-        loss = 0
         weight = 1
         loss = weight * self.loss['CTCLoss'](ret_dict["conv_out"].log_softmax(-1),
                                                       label.cpu().int(), ret_dict["conv_len"].cpu().int(),
